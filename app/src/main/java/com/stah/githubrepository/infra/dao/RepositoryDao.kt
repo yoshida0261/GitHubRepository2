9b259010c22df9af43f22c77b4beb8cb1cfc3596
yoshida0261 <yoshida261@gmail.com>
2019-05-22 03:15:54 +0900

    feat: add dao interface

---
+++ app/src/main/java/com/stah/githubrepository/infra/dao/RepositoryDao.kt
@@ -0,0 +1,15 @@
+package com.stah.githubrepository.infra.dao
+
+import com.stah.githubrepository.domain.entity.Repository
+
+class RepositoryDao() {
+
+    fun insert(repositorys: List<Repository>) {
+
+    }
+/*
+    fun findAll(): List<Repository> {
+
+    }
+    */
+}
---
+++ app/src/main/java/com/stah/githubrepository/presentation/presentor/MainPresentor.kt
@@ -0,0 +1,12 @@
+package com.stah.githubrepository.presentation.presentor
+
+import io.reactivex.disposables.CompositeDisposable
+
+
+class MainPresentor{
+    private val disposables = CompositeDisposable()
+
+    fun getMixiRepository(){
+
+    }
+}
