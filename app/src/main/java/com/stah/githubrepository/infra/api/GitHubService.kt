bfa96b77f269fea926cbfcf2b41412f427b8f0af
yoshida0261 <yoshida261@gmail.com>
2019-05-27 02:30:21 +0900

    refactor: modified api return Single

--- app/src/main/java/com/stah/githubrepository/infra/api/GitHubService.kt
+++ app/src/main/java/com/stah/githubrepository/infra/api/GitHubService.kt
@@ -2,11 +2,11 @@ package com.stah.githubrepository.infra.api
 
 import retrofit2.http.GET
 import retrofit2.http.Path
-import rx.Observable
+import rx.Single
 
 
 interface GitHubService {
 
     @GET("orgs/{org}/repos")
-    fun getRepositoty(@Path("org") org: String) : Observable<List<RepositoryJson>>
+    fun getRepositoty(@Path("org") org: String) : Single<List<RepositoryJson>>
 }
